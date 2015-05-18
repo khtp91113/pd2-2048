8c60d1cd192277c42ab43e0aa32cff98bd719c9e
khtp91113 <f74036459@mail.ncku.edu.tw>
2015-05-19 03:39:23 +0800

    resend again because last pull change my codes...

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-<<<<<<< HEAD
 #include "game.h"
 #include <QApplication>
 
@@ -13,7 +12,7 @@ int main(int argc, char *argv[])
     int height=dw.height()*0.75;
     if(width>height)
         width=height;
-    else if(width<height)
+    else
         height=width;
     w.setMinimumSize(width,height);
     w.setWindowTitle("2048");
@@ -23,35 +22,6 @@ int main(int argc, char *argv[])
     m->getmain(w);
 
     w.show();
-=======
-#include "ui_mainwindow.h"
-#include "proj_2048.h"
-#include <QApplication>
-#include <QtGui>
-#include <QtCore>
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow *w=new MainWindow;
-    w->setStyleSheet("#MainWindow{border-image:url(:/background/back.jpg);}");
-    w->setWindowTitle("2048");
-    QDesktopWidget dw;
-    int width=dw.width()*0.7;
-    int height=dw.height()*0.7;
-    w->setMinimumSize(width,height);
-    w->setFocus();
-    w->show();
-    w->set_check();
-    w->random_generate();
-    w->random_generate();
-
-
->>>>>>> c3bafb205848e51123afdaba602d6ba05f5d7812
-
-
     return a.exec();
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> c3bafb205848e51123afdaba602d6ba05f5d7812
